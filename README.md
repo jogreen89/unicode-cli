0865b2e396fcd41fe8a57104fd385c304f632263
zubernetes <zubernetes@gmail.com>
2018-01-25 22:03:55 -0500

    adding argparse functions

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # unicode-cli
 expermenting with multi-language character sets in Unicode
+
+..
+
+although, i'm actually going to be dumping a whole lot of other stuff in
+this repo, in the end. there may be work regarding whales that appears here
--- main.py
+++ main.py
@@ -1,17 +1,47 @@
-from flask import Flask
-app = Flask(__name__)
+import click
+import argparse
+from pdb import set_trace
+#from flask import Flask
+#app = Flask(__name__)
 
-def create_app(config_filename):
-    app = Flask(__name__)
-    app.config.from_pyfile(config_filename)
+#def create_app(config_filename):
+#    app = Flask(__name__)
+#    app.config.from_pyfile(config_filename)
 
-    from unicode_cli.model import db
-    db.init_app(app)
+#    from unicode_cli.model import db
+#    db.init_app(app)
+#    return
 
-@app.route("/")
+def greeting():
+	""" greeting - process the command line arguments """
+	parser = argparse.ArgumentParser(description="Process whale data")
+	parser.add_argument("--data", help="csv data to ingest")
+
+	args = parser.parse_args()
+	set_trace()
+	print(args)
+	return
+
+def process_data():
+	""" process_data - processes the incoming csv data """
+	return "Process Data func"
+
+"""
+@click.command()
+@click.option("--count", default=1, help="Number of greetings")
+@click.option("--name", prompt="Your name",
+              help="The person to greet.")
+def hello(count, name):
+    for x in range(count):
+        click.echo("Hello %s!" % name)
+"""
+
+# @app.route("/")
+
 def run():
     return "Hello, World"
 
 if __name__ == "__main__":
-    run()
+    # hello()
+	greeting()
 
